33600c72a4986738530b50be97cb1d076575d5f2
Olga Maksimova <lolik7447@gmail.com>
2024-09-20 19:58:09 +0300

    add files

---
+++ src/main/kotlin/lesson2/hw2.kt
@@ -0,0 +1,7 @@
+package com.olga.lesson2
+
+
+fun main() {
+    val name = "Hello world"
+    println(name)
+}
---
+++ src/main/kotlin/lesson5/lesson5.kt
@@ -0,0 +1,18 @@
+package com.olga.lesson5
+
+//fun main() {
+//    val isGreater = (5 > 3)
+//    val isLesser = (5 < 3)
+//    val isGreaterOrEqual = (5 >= 5) // <=
+//    val andResult = true && false // возвращает true только если в обоих случаях true
+//    val orResult = true || false // возвращает true если в одном из них true
+//    val notResult = !(5 > 3) // делает операцию обратной
+//    var number = 5
+//    number += 3
+//    number /= 2
+//
+//
+//    println(number--) // вычисление будет после печати, значит надо печатать еще раз
+//    println(++number) // вычисление до печати --/++ прибавляет +1 или убавляет -1
+//
+//}
